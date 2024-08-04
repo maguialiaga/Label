346aafa37ab8d373a4ca0828b59a48297972e1ef
maguialiaga <maguialiaga1@gmail.com>
2024-08-04 23:49:27 +0200

    color navbar

--- src/pages/roaster.js
+++ src/pages/roaster.js
@@ -6,7 +6,7 @@ import ImageCardList from "../components/ImageCardList";
 const roaster = () => {
   return (
     <Layout>
-      <NavBar navColor={"white"} />
+      <NavBar navColor={"black"} />
       <ImageCardList />
     </Layout>
   );
